cb76aa9e980ad67997c58d90915397ef13e00be6
Abisola <abisolaonileimo@yahoo.com>
2024-02-05 14:49:14 +0100

    added a function

--- main.py
+++ main.py
@@ -2,37 +2,64 @@
 # collecting Weather data from openWeather
 import requests
 
-# parameters
-latitude = 6.6018
-longitude = 3.3515
+# api key
 API_KEY = "79b5569d5e44702ed6ba125b2af265d5"
 
-# url
-weather_api = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}"
+def get_lat_lon(city_name):
+    """"Given a location return the latitude and longitude"""
+    try:
+        geo_api = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&appid={API_KEY}"
+        response = requests.get(url = geo_api).json()
+        lat = response[0]['lat']
+        long = response[0]['lon']
+    except:
+        print("Couldnt connect to API.")
+    return (long, lat)
 
-# make a request
-request = requests.get(weather_api).json()
+    
+# get data
+def get_weather_data(latitude, longitude):
+    """Given a latitude and longitude data return the data"""
+    weather_api = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}"
+    response = requests.get(url = weather_api).json()
 
-# data collection
-data = {
-    'country': request['sys'] ['country'],
-    'city': request['name'],
-    'longitude': request['coord']['lon'],
-    'latitude': request['coord']['lat'],
-    'temp': request['main']['temp'],
-    'temp_min': request['main']['temp_min'],
-    'temp_max': request['main']['temp_max'],
-    'pressure': request['main']['pressure'],
-    'humidity': request['main']['humidity'],
-    'sea_level': request['main']['sea_level'],
-     'ground_level': request['main']['grnd_level'],
-    'wind_speed': request['wind']['speed'],
-    'wind_degree': request['wind']['deg'],
-    'sunrise': request['sys']['sunrise'],
-    'sunset': request['sys']['sunset'],
-    'description': request['weather'][0]['description']
+    data = {
+    'country': response['sys'] ['country'],
+    'city': response['name'],
+    'longitude': response['coord']['lon'],
+    'latitude': response['coord']['lat'],
+    'temp': response['main']['temp'],
+    'temp_min': response['main']['temp_min'],
+    'temp_max': response['main']['temp_max'],
+    'pressure': response['main']['pressure'],
+    'humidity': response['main']['humidity'],
+    'sea_level': response['main']['sea_level'],
+     'ground_level': response['main']['grnd_level'],
+    'wind_speed': response['wind']['speed'],
+    'wind_degree': response['wind']['deg'],
+    'sunrise': response['sys']['sunrise'],
+    'sunset': response['sys']['sunset'],
+    'description': response['weather'][0]['description']
 } 
+    return (data)
+    
+    
+
+
+
+# # url
+# weather_api = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}"
+
+# # make a request
+# request = requests.get(weather_api).json()
+
+# # data collection
+
 
 
 # status
-print(data)
+long = get_lat_lon("Abuja")[0]
+lat = get_lat_lon("Abuja")[1]
+
+
+print(get_weather_data(latitude=lat, longitude=long))
